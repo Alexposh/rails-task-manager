fe4410fe7992d8c3707f6f7c4dc9160e2cc71d3d
Alexandru Posirca <alexposh@gmail.com>
2024-11-12 15:55:36 +0000

    Tasks done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks/new", to: "tasks#new", as: :new
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
+
 end
